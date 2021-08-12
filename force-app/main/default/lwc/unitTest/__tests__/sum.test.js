c61262ead282ce4b7a15badc08e44045367a9de2
gokulrajanoff <gokulrajanoff@gmail.com>
2021-08-12 20:11:41 +0530

    delete unit test

--- force-app/main/default/lwc/unitTest/__tests__/sum.test.js
+++
@@ -1,7 +0,0 @@
-import { sum } from '../sum';
-    
-describe('summation of 2 numbers',()=>{
-    it("testsum",()=>{
-        expect(sum(1,1)).toBe(2);
-    })
-})
--- force-app/main/default/lwc/unitTest/unitTest.js
+++
@@ -1,9 +0,0 @@
-import { LightningElement, api } from 'lwc';
-import { sum } from './sum';
-  
-export default class UnitTest extends LightningElement {
-  @api unitNumber = sum(2,3);
-  handleChange(event) {
-    this.unitNumber = event.target.value;
-  }
-}
